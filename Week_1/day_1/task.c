09e9f4356c0aa1e3b6d75428de3ed4c77a0f5ff2
Mohamed Abdelwahed <memoelfares613@gmail.com>
2023-11-09 12:39:30 +0200

    finished week_1 task_1 tasks today 9/11/2023

--- Week_1/day_1/task.c
+++ Week_1/day_1/task.c
@@ -32,5 +32,32 @@ void quotient_and_remainder(int num1 , int num2){
     printf("Remainder of \"%i\" and \"%i\" is : %i\n" , num1 , num2 , num1 % num2);
 }
 /**================================================*/
+void personal_data(void){
+    char personal_Id[14];
+    char name[50];
+    int age = 0 ;
+    char department[40];
+    char fav_hobby[30];
 
+    printf("\n================= Enter your data : =====================\n");
+    printf("Enter Personal Id : ");
+    gets(personal_Id);
+    printf("Enter Name : ");
+    gets(name);
+    printf("Enter Age : ");
+    scanf("%i" , &age );
+    fflush(stdin);
+    printf("Enter Department : ");
+    gets(department);
+    printf("Enter Favourite Hobby : ");
+    gets(fav_hobby);
+    
+    printf("\n================== Data you Enterd is : ====================\n");
+    printf("Personal Id is : %s\n" , personal_Id);
+    printf("=>==> Name is : %s\n" , name);
+    printf("=>==> Personal Id is : %i\n" , age);
+    printf("=>==> Personal Id is : %s\n" , department);
+    printf("=>==> Personal Id is : %s\n" , fav_hobby);
+
+}
 /**================================================*/
--- Week_1/day_1/task.h
+++ Week_1/day_1/task.h
@@ -4,3 +4,4 @@ float average_3_numbers(float num1 , float num2 , float num3);
 void display_datatypes(int integer_num, float  float_num  , char char_num , long long_num , double double_num );
 void swap_num(int *num1 , int *num2);
 void quotient_and_remainder(int num1 , int num2);
+void personal_data(void);
