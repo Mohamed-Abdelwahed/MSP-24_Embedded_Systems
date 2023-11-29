6e477cd667ac7f9e347b636f49084973daf448c5
Mohamed-Abdelwahed <memoelfares613@gmail.com>
2023-11-29 18:12:47 +0200

    finish task_2 29/11/2023

--- Week_2/day_1/task.c
+++ Week_2/day_1/task.c
@@ -43,5 +43,105 @@ void even_or_odd(int num){
 
 }
 /*=================================================*/
+char nth_bit(int num , int bit_num){
+    return ((num >> bit_num) & 1);
+}
+/*=================================================*/
+int set_bit(int num , int bit_num){
+    return (num | (1 << (bit_num - 1))); 
+}
+/*=================================================*/
+int clearBit(int num, int bit_num) 
+{ 
+    return (num & (~(1 << (bit_num - 1)))); 
+} 
+/*=================================================*/
+int toggleBit(int num, int bit_num) 
+{ 
+    return (num ^ (1 << (bit_num - 1))); 
+} 
+/*=================================================*/
+void compare_using_ternary(int num_1 , int num_2){
+    {
+        num_1 > num_2 ?printf("\n===> %i is bigger than  %i\n" , num_1 , num_2) : num_2 > num_1 ? printf("\n===> %i is bigger than  %i\n" , num_2 , num_1) : printf("\n===> %i is equal   %i\n" , num_1 , num_2); 
+    }
+}
+/*=================================================*/
+void leap_year(int year){
+    if (year % 400 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   else if (year % 100 == 0) {
+      printf("%d is not a leap year.", year);
+   }
+   else if (year % 4 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   else {
+      printf("%d is not a leap year.", year);
+   }
+}
+/*=================================================*/
+void even_odd(int num){
+    if (num % 2 == 0)
+    {
+        printf("\n====>number %i is even \n" , num);
+    }else{
+                printf("\n====>number %i is odd \n" , num);
+
+    }
+    
+}
+/*=================================================*/
+void largest_num(float n1 , float n2 ,float n3){
+    if(n1 >= n2){
+        if (n1 >= n3)
+        {
+               printf("%.2f is the largest number.", n1);
+
+        }else if(n3 >= n2){
+             printf("%.2f is the largest number.", n3);
+        }else{
+            printf("%.2f is the largest number.", n2);
+        }
+        
+    }else if(n2 > n3)
+    {
+                    printf("%.2f is the largest number.", n2);
+    }else{
+                     printf("%.2f is the largest number.", n3);
+
+    }
+    
+    
+}
+/*=================================================*/
+    void grad_result(int n1 , int n2 ,int n3 , int n4 , int n5){
+        int  percent = (n1+n2+n3+n4+n5)/5;
+        if(percent > 80){
+            printf("A+");
+        }else if(percent >= 65 && percent <= 80)
+        {
+                        printf("A");
+
+        }else if(percent >= 50 && percent <= 56)
+        {
+                        printf("B");
+
+        }else if(percent >= 42 && percent <= 50)
+        {
+                        printf("C");
+
+        }else if(percent <= 42)
+        {
+                        printf("Fail");
+
+        }else 
+        {
+                        printf("Please Repeat and eneter valid numbers \n");
+
+        }
+        
+    }
 /*=================================================*/
 /*=================================================*/
